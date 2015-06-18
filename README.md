8b6d99d5f31ef02931c26f9dcb8b848ba20ae598
Manuel Vila <mvila@3base.com>
2015-06-18 09:57:16 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# kinda-application [![Build Status](https://travis-ci.org/kinda/kinda-application.svg?branch=master)](https://travis-ci.org/kinda/kinda-application)
+
+```js
+// TODO: documentation!
+```
---
+++ src/index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+let _ = require('lodash');
+let KindaObject = require('kinda-object');
+let KindaEventManager = require('kinda-event-manager');
+let KindaLog = require('kinda-log');
+let KindaLocalizer = require('kinda-localizer');
+
+let KindaApplication = KindaObject.extend('KindaApplication', function() {
+  this.include(KindaEventManager);
+
+  this.creator = function(options = {}) {
+    _.assign(this, _.pick(options, [
+      'name', 'displayName', 'version'
+    ]));
+
+    let log = options.log;
+    if (!KindaLog.isClassOf(log)) {
+      log = KindaLog.create(log);
+    }
+    this.log = log;
+
+    let localizer = options.localizer;
+    if (!KindaLocalizer.isClassOf(localizer)) {
+      localizer = KindaLocalizer.create(localizer);
+    }
+    this.localizer = localizer;
+
+    Object.defineProperty(this, 'locale', {
+      get() {
+        if (!this._locale) this._locale = this.localizer.getLocale();
+        return this._locale;
+      },
+      set(locale) {
+        if (_.isString(locale)) locale = this.localizer.getLocale(locale);
+        if (this._locale !== locale) {
+          this._locale = locale;
+          this.emit('locale.didChange', locale);
+        }
+      }
+    });
+  };
+});
+
+module.exports = KindaApplication;
